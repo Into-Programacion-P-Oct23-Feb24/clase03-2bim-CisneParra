768c5c040d84fa0f29ba765d240d2f5251e8b113
CisneParra <adparra2@utpl.edu.ec>
2023-12-20 15:41:51 -0500

    cambios de la clase

---
+++ Ejemplosc3/src/paquete05/ejemplo001.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author UTPL
+ */
+public class ejemplo001 {
+    public static void main(String[] args) {
+        int[] arreglo3 = new int[2];
+        for (int i = 0; i < arreglo3.length; i++ ){
+            System.out.printf("%d\n", arreglo3[i]);
+        }
+    }
+    
+}
