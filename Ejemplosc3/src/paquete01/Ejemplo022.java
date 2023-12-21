adf297b37be9b3d1f569c937be00b1247166af0f
Cisne Parra <adparra2@utpl.edu.ec>
2023-12-20 21:54:40 -0500

    cambios de la tarea

--- Ejemplosc3/src/paquete01/Ejemplo022.java
+++ Ejemplosc3/src/paquete01/Ejemplo022.java
@@ -18,34 +18,33 @@ public class Ejemplo022 {
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
+
         // forma 1
         Scanner entrada = new Scanner(System.in);
         int[][] arreglo1 = new int[3][4]; //
-       // System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
-        
+        // System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
+
         // System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]);
-       
         arreglo1[0][2] = 1000;
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
                 System.out.println("ingrese un numero");
                 int valor = entrada.nextInt();
-                if ((valor % 2 == 0) && (valor >= 10)){
-                arreglo1[fila][col] = 0;
-            }else {
-                arreglo1[fila][col] = valor;
+                if ((valor % 2 == 0) && (valor >= 10)) {
+                    arreglo1[fila][col] = 0;
+                } else {
+                    arreglo1[fila][col] = valor;
                 }
             }
         }
-               
+
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
                 System.out.printf("fila[%d] columna[%d] = %d\n",
                         fila, col, arreglo1[fila][col]);
             }
         }
-        
+
     }
-    
+
 }
--- Ejemplosc3/src/paquete05/Ejemplo001.java
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -14,6 +14,6 @@ public class Ejemplo001 {
         for (int i = 0; i < arreglo3.length; i++ ){
             System.out.printf("%d\n", arreglo3[i]);
         }
-    }//.
+    }
     
 }
